da1e7afa282af08bca881dcf131e2b9bab2f7deb
Aryan1092raj <aryanraj1092@gmail.com>
2025-10-24 01:40:19 +0530

    Uploading To GitHub

---
+++ Readme.md
@@ -0,0 +1,22 @@
+#First, I started from arduino basics lecture - 
+I learnt diffirent Arduino Syntax like pinMode,digitalWrite,analogRead , etc.
+and learn about datatypes  and comaprison and logical operators
+Loops , switch-case  and write code for a blinking a LED
+
+
+#Then , I started learning the algorithm for maze solver, starting from dijkstra  to flood fill ,left/right wall following algos
+i have understood them but i can not figure out how to use them,especially dijkstra and flood fill 
+
+#Them i learn about PCBs - Basics and PID controller but i have used it to write my Line Following bot code
+as i am not fully understood it yet
+
+#writing code, after  a while i figure out that in if-elif statements ,
+i should check for straight path first ,then for sharp left/right and then slight turns as 
+if for example but straight and slight left and also slight right sensors get 1 as input , it means we have to move straight for sure
+thats why i should check it first. 
+
+#Thing for i used AI --
+for pin configuration on code
+extension for arduino code
+arranging all links provived to study in a sequence for flow and better understanding.
+
---
+++ lineFollowing.ino
@@ -0,0 +1,58 @@
+const int pins[5] = {2,3,4,5,6};
+const int leftForward = 9 ;
+const int rightForward = 10 ;
+
+void setup(){
+    for(int i = 0 ; i < 5 ; ++i){
+        pinMode(pins[i],INPUT);
+    }
+    pinMode(leftForward,OUTPUT);
+    pinMode(rightForward,OUTPUT);
+}
+
+void loop(){
+    int sensorValue[5];
+    for(int i = 0 ; i < 5 ; ++i){
+        sensorValue[i] = digitalRead(pins[i]);
+    }
+
+    if(sensorValue[0] == 1){
+        sharpLeft();
+    }else if(sensorValue[1] == 1){
+        slightLeft();
+    }else if(sensorValue[2] == 1){
+        straight();
+    }else if(sensorValue[3] == 1){
+        slightRight();
+    }else if(sensorValue[4] == 1){
+        sharpRight();
+    }else{
+        stop();
+    }    
+}
+
+void straight(){
+    analogWrite(leftForward,180);
+    analogWrite(rightForward,180);
+}
+
+void sharpLeft(){
+    analogWrite(leftForward,0);
+    analogWrite(rightForward,180);
+}
+void slightLeft(){
+    analogWrite(leftForward,130);
+    analogWrite(rightForward,180);
+}
+void sharpRight(){
+    analogWrite(leftForward,180);
+    analogWrite(rightForward,0);
+}
+void slightRight(){
+    analogWrite(leftForward,180);
+    analogWrite(rightForward,130);
+}
+void stop(){
+    analogWrite(leftForward,0);
+    analogWrite(rightForward,0);
+}
